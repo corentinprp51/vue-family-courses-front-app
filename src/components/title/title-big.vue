8ca5e7001222eb80733874e8fc9880c35b17b9ab
corentinprp51 <corentinprp@gmail.com>
2022-07-19 17:02:40 +0200

    feat: add title components

---
+++ src/components/title/title-big.vue
@@ -0,0 +1,11 @@
+<template>
+  <title-template>
+    <h1 class="text-[48px] font-semibold leading-[50px]">
+      <slot />
+    </h1>
+  </title-template>
+</template>
+
+<script lang="ts" setup>
+import TitleTemplate from '@/components/title/title-template.vue';
+</script>
